ab7f1e2b4a5256a1d7cf1cd7cd70d59271376454
Ramil Sagidullin <rasagidullin@avito.ru>
2024-06-26 00:02:41 +0300

    feat: removed system instructions & endpoints

--- index.js
+++ index.js
@@ -32,27 +32,6 @@ const generationConfig = {
   temperature: 1,
 };
 
-const getSystemInstructions = (mode) => {
-  let result;
-
-  if (mode === 'create') {
-    result = 'писать текст для постов на заданную тему с определенным описанием! \n';
-  } else if (mode === 'edit') {
-    result = 'отредактировать текст по заданным инструкциям! \n';
-  }
-
-  return (
-    'Ты копирайтер и твоя задача ' +
-    result +
-    'Правила написания текста: \n' +
-    '1) Структура: Каждый текст должен иметь четкую структуру: вводную часть, основное тело и заключение. Используй подзаголовки, списки и выделение ключевых моментов для улучшения читаемости. \n' +
-    '2) Полезность и осмысленность: Контент должен приносить ценность читателю. Это может быть решение проблемы, ответ на вопрос, новая информация или идеи для вдохновения. Убедись, что каждый текст содержит конкретные советы, рекомендации или выводы. \n' +
-    '3) Привлекательность: Для увеличения привлекательности текста используй интересные факты, примеры из жизни, цитаты экспертов и визуальные элементы (если это возможно). Включай вопросы, которые могут вызвать интерес и желание прочитать текст до конца. \n' +
-    '4) Оригинальность: Стремись к тому, чтобы контент был уникальным и отличался от того, что уже доступно в интернете. Избегай клише и общих фраз. \n' +
-    '5) SEO-оптимизация: Включи ключевые слова, соответствующие теме текста, но делай это естественно, не нарушая читаемость и качество контента. '
-  );
-};
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -126,7 +105,6 @@ app.post('/getContent', async (req, res) => {
       model: 'gemini-1.5-flash-latest',
       generationConfig,
       safetySettings,
-      // systemInstruction: getSystemInstructions(mode),
     });
 
     const result = await gemini.generateContentStream([getPrompt()]);
@@ -141,71 +119,24 @@ app.post('/getContent', async (req, res) => {
     const { error } = await supabase
       .from('generations')
       .insert([
-        { mode, topic, description, text, keywords, style, tone, language, content, userId },
+        {
+          mode,
+          topic,
+          contentType,
+          targetAudience,
+          description,
+          text,
+          keywords,
+          style,
+          tone,
+          language,
+          content,
+          userId,
+        },
       ]);
 
     if (error) {
       throw error;
-    }
-
-    res.end();
-  } catch (e) {
-    console.log('POST getContent:', e.message);
-    res.json('Something went wrong');
-  }
-});
-
-app.post('/analyzeOptimization', async (req, res) => {
-  try {
-    const { text } = req.body;
-
-    const getPrompt = () => {
-      return `Детально проанализируй текст на предмет SEO-оптимизации, включая проверку на наличие ключевых слов, их плотность, читабельность текста, длину предложений и абзацев: \n ${text}`;
-    };
-
-    const gemini = googleGenAI.getGenerativeModel({
-      model: 'gemini-1.5-flash-latest',
-      generationConfig,
-      safetySettings,
-    });
-
-    const result = await gemini.generateContentStream([getPrompt()]);
-    let content = '';
-
-    for await (const chunk of result.stream) {
-      const message = chunk.text();
-      content += message;
-      res.write(message);
-    }
-
-    res.end();
-  } catch (e) {
-    console.log('POST getContent:', e.message);
-    res.json('Something went wrong');
-  }
-});
-
-app.post('/analyzeTonality', async (req, res) => {
-  try {
-    const { text } = req.body;
-
-    const getPrompt = () => {
-      return `Детально проанализируй тональность текста и определи эмоциональную окраску: \n ${text}`;
-    };
-
-    const gemini = googleGenAI.getGenerativeModel({
-      model: 'gemini-1.5-flash-latest',
-      generationConfig,
-      safetySettings,
-    });
-
-    const result = await gemini.generateContentStream([getPrompt()]);
-    let content = '';
-
-    for await (const chunk of result.stream) {
-      const message = chunk.text();
-      content += message;
-      res.write(message);
     }
 
     res.end();
