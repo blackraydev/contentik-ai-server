0c3c61862f65db1890b0de5b43b196fd24afe0af
Ramil Sagidullin <rasagidullin@avito.ru>
2024-07-22 21:31:06 +0300

    feat: added real db

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
-const { Sequelize, Op } = require('sequelize');
-const sequelize = new Sequelize('postgres', 'postgres', '', {
-  host: 'localhost',
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_URL, {
   dialect: 'postgres',
   logging: false,
 });
