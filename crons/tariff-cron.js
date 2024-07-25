71b62b58f594d0188d79d66af734a68a3e974adf
Ramil Sagidullin <rasagidullin@avito.ru>
2024-07-25 18:49:33 +0300

    test: cron every minute

--- crons/tariff-cron.js
+++ crons/tariff-cron.js
@@ -2,7 +2,8 @@ const cron = require('node-cron');
 const tariffService = require('../services/tariff-service');
 
 const scheduleTariffSubscriptionCheckoutCron = () => {
-  cron.schedule('0 */12 * * *', async () => {
+  // cron.schedule('0 */12 * * *', async () => {
+  cron.schedule('* * * * *', async () => {
     console.log('Проверка подписок...');
     await tariffService.checkoutSubscriptionTariff();
   });
