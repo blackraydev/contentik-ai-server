28162a81c5543394bbbe11fe87690a97d4612fe4
Ramil Sagidullin <rasagidullin@avito.ru>
2024-07-26 15:50:37 +0300

    feat: removed https & added url in cors whitelist

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const fs = require('fs');
 const http = require('http');
-const https = require('https');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -13,18 +12,7 @@ const errorMiddleware = require('./middlewares/error-middleware');
 const { scheduleTariffSubscriptionCheckoutCron } = require('./crons/tariff-cron');
 
 const HTTP_PORT = process.env.HTTP_PORT || 8080;
-const HTTPS_PORT = process.env.HTTPS_PORT || 4443;
 const app = express();
-
-const privateKey = fs.readFileSync('/etc/ssl/contentik/contentik.key', 'utf8');
-const certificate = fs.readFileSync('/etc/ssl/contentik/contentik.crt', 'utf8');
-const ca = fs.readFileSync('/etc/ssl/contentik/ca.crt', 'utf8');
-
-const credentials = {
-  key: privateKey,
-  cert: certificate,
-  ca: ca,
-};
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -33,8 +21,8 @@ app.use(cookieParser());
 app.use((req, res, next) => {
   const corsWhiteList = [
     'https://contentik-ai.ru',
+    'https://app.contentik-ai.ru',
     'https://localhost:5173',
-    'http://localhost:5173',
   ];
 
   if (corsWhiteList.includes(req.headers.origin)) {
@@ -52,16 +40,10 @@ app.use((req, res, next) => {
 app.use('/api', router);
 app.use(errorMiddleware);
 
-// Starting both http & https servers
 const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
 
-httpServer.listen(HTTP_PORT, () => {
-  console.log(`HTTP Server running on port ${HTTP_PORT}`);
-});
-
-httpsServer.listen(HTTPS_PORT, async () => {
+httpServer.listen(HTTP_PORT, async () => {
   await sequelize.authenticate();
   scheduleTariffSubscriptionCheckoutCron();
-  console.log(`HTTPS Server running on port ${HTTPS_PORT}`);
+  console.log(`HTTP Server running on port ${HTTP_PORT}`);
 });
