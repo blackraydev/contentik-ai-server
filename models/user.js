3c346e2e23e6fdbd0a7c5d5b6d30eb763c72d991
Ramil Sagidullin <rasagidullin@avito.ru>
2024-08-06 20:08:47 +0300

    feat: email is not required

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ module.exports = (sequelize) => {
     },
     email: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
       unique: true,
     },
     password: {
